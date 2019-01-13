e89517d9f381763e4f54e2aa948043dba8c3979a
philipp_ludersdorfer <philipp.ludersdorfer@gmail.com>
2019-01-13 20:36:13 +0000

    added calibration plot

--- ml_helpers/plots/__init__.py
+++ ml_helpers/plots/__init__.py
@@ -2,5 +2,5 @@ from ml_helpers.plots.evaluation import (
     roc_curve,
     precision_recall_curve,
     class_separation,
-
+    calibration_curve
 )
--- ml_helpers/plots/evaluation.py
+++ ml_helpers/plots/evaluation.py
@@ -88,26 +88,61 @@ def class_separation(y_true, y_score, bins = 30, alpha = 0.7, colors = ['oranger
     plt.show()
 
 
-def reliability_curve(y_true, y_score, bins = 10, color='dodgerblue', lw=2):
-    """Plots the distribution"""
+def calibration_curve(y_true, y_score, calibrate=None, clf=None, x=None, cv=None, bins=10, color='dodgerblue', lw=2):
+    """Plots the probability calibration curve.
 
+    The plot shows the fraction of true positives as a function of mean predicted score per score bin.
+    The number of examples per score bin (histogram) is plotted on a second vertical axis.
+
+    Args:
+        y_true - (array) true class labels
+        y_score - (array) predicted scores
+        calibrate - (str) 'sigmoid', 'isotomic', or None
+        clf - (object) scikit-Learn Classifier object
+        cv - (int) number of cross-validation folds for calibration
+        bins - (int) number of bins
+        color - (list) colors for class labels
+        lw - (int) line width
+    Returns a plot with the probability calibration curve and a histogram of predicted scores.
+    """
+    # compute brier score
     brier_score = metrics.brier_score_loss(y_true, y_score, pos_label=y_true.max())
-
+    # compute fraction of true positives and mean predicted score per bin
     prop_true, prop_pred = calibration.calibration_curve(y_true=y_true,
                                                          y_prob=y_score,
                                                          n_bins=bins)
 
-    plt.figure()
+    # calibration
+    if calibrate:
+        calibrated_clf = calibration.CalibratedClassifierCV(clf, cv=cv, method=calibrate)
+        calibrated_clf.fit(x,y_true)
+        y_score_cal = calibrated_clf.predict_proba(x)[:,1]
+        brier_score_cal = metrics.brier_score_loss(y_true, y_score_cal, pos_label=y_true.max())
+        # compute fraction of true positives and mean predicted score per bin
+        prop_true_cal, prop_pred_cal = calibration.calibration_curve(y_true=y_true,
+                                                                     y_prob=y_score_cal,
+                                                                     n_bins=bins)
+
     # create plot
     plt.figure()
-    # Draw precision-recall curve
-    plt.plot(prop_pred, prop_true, color=color, lw=lw, label='Brier score = %0.2f)' % brier_score)
+    # draw reliability curve
+    plt.plot(prop_pred, prop_true, color=color, lw=lw, label='Uncalibrated (Brier score = %0.2f)' % brier_score)
+    # draw calibrated reliability curve
+    if calibrate:
+        plt.plot(prop_pred_cal, prop_true_cal, color='purple', lw=lw,
+                 label='Calibrated (Brier score = %0.2f)' % brier_score_cal)
     # draw reference line
     plt.plot([0, 1], [0, 1], color='black',lw=lw, linestyle='--')
+    # label axes
     plt.ylabel('Fraction of positives')
     plt.xlabel('Predicted score')
+    plt.legend(loc='lower right')
+    # add second vertical axis
     plt.twinx()
-    plt.hist(y_score, bins=30, alpha=0.3, color=color)
+    # draw predicted score histogram
+    plt.hist(y_score, bins=bins, alpha=0.5, histtype='step', color=color)
+    if calibrate:
+        plt.hist(y_score_cal, bins=bins, alpha=0.5, histtype='step', color='purple')
     plt.ylabel('Frequency')
     plt.grid()
     plt.title('Calibration Plot (Reliability Curve)')
